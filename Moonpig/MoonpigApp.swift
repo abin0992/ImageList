79fb4d55bce3082c0eeac407fe330ac48e7ba6de
Abin Baby <abin0992@gmail.com>
2024-01-16 21:19:18 +0100

    Initial Commit

---
+++ Moonpig/MoonpigApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonpigApp.swift
+//  Moonpig
+//
+//  Created by Abin Baby on 16.01.24.
+//
+
+import SwiftUI
+
+@main
+struct MoonpigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
